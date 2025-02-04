153e98a04058a580a3e0484e6261a5976424e511
German Bustamante <germanbustamante.trabajo@gmail.com>
2025-02-04 02:13:39 +0100

    [RM-1.0.0] Improved BottomNavigation.kt and BottomScreens with selected/unselected icons, improved UX and UI, and, divided BottomNavigation() in smaller methods to improve readability

--- app/src/main/java/com/germandebustamante/ringtonemanager/core/navigation/BottomNavigation.kt
+++ app/src/main/java/com/germandebustamante/ringtonemanager/core/navigation/BottomNavigation.kt
@@ -1,6 +1,8 @@
 package com.germandebustamante.ringtonemanager.core.navigation
 
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -8,11 +10,15 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -20,52 +26,118 @@ import com.germandebustamante.ringtonemanager.core.navigation.bottom.screen.Bott
 
 @Composable
 fun AppBottomNavigation(navController: NavController) {
-    val topLevelRoutes = listOf(
-        BottomScreens.Home,
-        BottomScreens.MyAccount,
-    )
+    val topLevelRoutes: List<BottomScreens<out Any>> = listOf(BottomScreens.Home, BottomScreens.MyAccount)
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentDestination = navBackStackEntry?.destination
-    val isTopLevelRoute = topLevelRoutes.any { it.route::class.qualifiedName == currentDestination?.route }
+    val isTopLevelRoute = isCurrentDestinationTopLevel(currentDestination, topLevelRoutes)
 
     if (isTopLevelRoute) {
-        NavigationBar(
-            containerColor = MaterialTheme.colorScheme.surface,
-            windowInsets = WindowInsets(top = 0.dp, bottom = 0.dp, left = 0.dp, right = 0.dp),
-        ) {
-            topLevelRoutes.forEach { topLevelRoute ->
-                val name = stringResource(topLevelRoute.name)
-                val isSelected =
-                    currentDestination?.hierarchy?.any { it.route == topLevelRoute.route::class.qualifiedName } == true
-                NavigationBarItem(
-                    icon = {
-                        Icon(
-                            painterResource(topLevelRoute.icon),
-                            contentDescription = name,
-                            tint = getSelectedColor(isSelected)
-                        )
-                    },
-                    label = { Text(name, color = getSelectedColor(isSelected)) },
-                    selected = currentDestination?.hierarchy?.any { it.route == topLevelRoute.route::class.qualifiedName } == true,
-                    onClick = {
-                        navController.navigate(topLevelRoute.route) {
-                            // Pop up to the start destination of the graph to
-                            // avoid building up a large stack of destinations
-                            // on the back stack as users select items
-                            popUpTo(navController.graph.findStartDestination().id) {
-                                saveState = false
-                            }
-                            launchSingleTop = false // Restore state when reselecting a previously selected item
-                            // Restore state when reselecting a previously selected item
-                            restoreState = true
-                        }
-                    }
+        BottomNavigationBar(navController, topLevelRoutes, currentDestination)
+    }
+}
+
+@Composable
+private fun BottomNavigationBar(
+    navController: NavController,
+    topLevelRoutes: List<BottomScreens<out Any>>,
+    currentDestination: NavDestination?,
+) {
+    val lineColor = MaterialTheme.colorScheme.onBackground
+
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        windowInsets = WindowInsets(bottom = 30.dp),
+        modifier = Modifier
+            .drawBehind {
+                drawLine(
+                    color = lineColor,
+                    strokeWidth = 1.dp.toPx(),
+                    start = Offset(0f, 0f),
+                    end = Offset(size.width, 0f),
                 )
             }
+            .heightIn(max = 110.dp)
+    ) {
+        topLevelRoutes.forEach { route ->
+            NavigationBarItem(
+                icon = { NavigationIcon(route, currentDestination) },
+                label = { NavigationLabel(route, currentDestination) },
+                selected = false, // False to avoid the default ripple effect
+                onClick = { navigateToRoute(navController, route) }
+            )
         }
     }
 }
 
 @Composable
+private fun NavigationLabel(route: BottomScreens<out Any>, currentDestination: NavDestination?) {
+    val isSelected = isRouteSelected(route, currentDestination)
+    val labelText = stringResource(route.name)
+
+    TextWithUnderline(text = labelText, underline = isSelected)
+}
+
+@Composable
+private fun NavigationIcon(route: BottomScreens<out Any>, currentDestination: NavDestination?) {
+    val isSelected = isRouteSelected(route, currentDestination)
+    val iconRes = if (isSelected) route.selectedIcon else route.unselectedIcon
+    val contentDescription = stringResource(route.name)
+
+    Icon(
+        painter = painterResource(iconRes),
+        contentDescription = contentDescription,
+        tint = getSelectedColor(isSelected)
+    )
+}
+
+@Composable
+private fun TextWithUnderline(text: String, underline: Boolean) {
+    val color = getSelectedColor(underline)
+    val modifier = if (underline) {
+        Modifier
+            .drawBehind {
+                val verticalOffset = size.height + 2.dp.toPx()
+                drawLine(
+                    color = color,
+                    strokeWidth = 2.dp.toPx(),
+                    start = Offset(0f, verticalOffset),
+                    end = Offset(size.width, verticalOffset),
+                )
+            }
+            .padding(horizontal = 4.dp)
+    } else {
+        Modifier
+    }
+
+    Text(
+        text = text,
+        color = color,
+        modifier = modifier
+    )
+}
+
+@Composable
 private fun getSelectedColor(isSelected: Boolean): Color =
     if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary
+
+private fun isCurrentDestinationTopLevel(
+    currentDestination: NavDestination?,
+    topLevelRoutes: List<BottomScreens<out Any>>,
+): Boolean {
+    return topLevelRoutes.any { it.route::class.qualifiedName == currentDestination?.route }
+}
+
+private fun isRouteSelected(
+    route: BottomScreens<out Any>,
+    currentDestination: NavDestination?,
+): Boolean = currentDestination?.hierarchy?.any { it.route == route.route::class.qualifiedName } == true
+
+private fun navigateToRoute(navController: NavController, route: BottomScreens<out Any>) {
+    navController.navigate(route.route) {
+        popUpTo(navController.graph.findStartDestination().id) {
+            saveState = false
+        }
+        launchSingleTop = false
+        restoreState = true
+    }
+}
