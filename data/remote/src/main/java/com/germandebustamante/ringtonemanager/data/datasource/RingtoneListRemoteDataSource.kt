5d2326cbb05f9d363d453fd4fcdc798d5cbca0d9
German Bustamante <germanbustamante.trabajo@gmail.com>
2025-02-07 16:50:26 +0100

    [RM-1.0.0] Improved FirestoreManager.getDocumentsFlow() to allow query instead of Task<QuerySnapshot> The purpose of this change to flow it's that when i change a ringtone (due to user action or changed in Firebase console) the layout updates automatically :)

--- data/remote/src/main/java/com/germandebustamante/ringtonemanager/data/datasource/RingtoneListRemoteDataSource.kt
+++ data/remote/src/main/java/com/germandebustamante/ringtonemanager/data/datasource/RingtoneListRemoteDataSource.kt
@@ -1,8 +1,10 @@
 package com.germandebustamante.ringtonemanager.data.datasource
 
+import arrow.core.Either
+import com.germandebustamante.ringtonemanager.domain.error.CustomError
 import com.germandebustamante.ringtonemanager.domain.ringtone.model.RingtoneBO
 import kotlinx.coroutines.flow.Flow
 
 interface RingtoneListRemoteDataSource {
-    fun getPopularRingtones(): Flow<List<RingtoneBO>>
+    fun getPopularRingtones(): Flow<Either<CustomError, List<RingtoneBO>>>
 }
